13a2d9c8de8f829f6ab21a25f2f8cde6cb4c65ab
happythe3 <alexshearer00@gmail.com>
2015-04-27 00:08:36 +0800

    Added main and #includes to testGame.c Uncommented some defaults in game.h

--- testGame.c
+++ testGame.c
@@ -1,2 +1,19 @@
 //Do I know how to use github???
 //YAY I KNOW GITHUD ;)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "game.h"
+
+int main(int argc, char *argv[]){
+	Game game;
+
+	game = newGame(DEFAULT_DISCIPLINES, DEFAULT_DICE);
+
+
+	assert(getTurnNumber(game) == -1);//When game is first gened turnNum should be -1
+
+	return EXIT_SUCCESS;
+}
