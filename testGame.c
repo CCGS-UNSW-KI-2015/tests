07e768c4619537405ca248f676b791f00c3a5aab
happythe3 <alexshearer00@gmail.com>
2015-04-27 19:45:36 +0800

    More tests

--- testGame.c
+++ testGame.c
@@ -20,10 +20,21 @@ int main(int argc, char *argv[]){
 	int defaultDice[] = DEFAULT_DICE;
 	game = newGame(defaultDis, defaultDice);
 
+	path startLocA1 = { 0 };
+
+	assert(getMostARCs(game) == NO_ONE);
+	assert(getMostPublications(game) == NO_ONE);
+	assert(getWhoseTurn(game) == NO_ONE);
+
+	int isLegalAction(Game g, action a);
+	action illegalAction1 = {OBTAIN_ARC, startLocA1, STUDENT_THD, STUDENT_THD};
+	assert(isLegalAction(game, illegalAction1) == FALSE);
 
 	assert(getTurnNumber(game) == -1);//When game is first gened turnNum should be -1
 	throwDice(game, 2);
 	assert(getTurnNumber(game) == 0);
+
+	
 
 	printf("All tests passed. You can kind of code :D\n");
 
