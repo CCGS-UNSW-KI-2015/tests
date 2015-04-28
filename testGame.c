c1b85ce6267ce080904c6571cf22d104d3179122
Jason Chu <jasonchujc@gmail.com>
2015-04-29 00:03:50 +0800

    Add more checks to initialisation

--- testGame.c
+++ testGame.c
@@ -14,27 +14,55 @@ int main(int argc, char *argv[]){
 	char temp = 'a';
 	scanf("%c", &temp);
 
-	//---------------TESTs-----------//
+	//--------------TESTS-----------//
 
 	int defaultDis[] = DEFAULT_DISCIPLINES;
 	int defaultDice[] = DEFAULT_DICE;
+	int universities[] = {UNI_A, UNI_B, UNI_C}
+	int defaultSize = NUM_REGIONS;
+	int universitiesSize = NUM_UNIS;
+	int i = 0;
+
 	game = newGame(defaultDis, defaultDice);
 
 	path startLocA1 = { 0 };
+
+	// Verify that everything has been loaded correctly
+
+	i = 0;
+	while (i < NUM_REGIONS) {
+		assert(defaultDice[i] == getDiceValue(i));
+		assert(defaultDis[i] == getDiscipline(i));
+		i++;
+	}
+
+	// Make sure the game state is intialised correctly
 
 	assert(getMostARCs(game) == NO_ONE);
 	assert(getMostPublications(game) == NO_ONE);
 	assert(getWhoseTurn(game) == NO_ONE);
 
+	i = 0;
+	while (i < universitiesSize) {
+		assert(getKPIpoints(game, universities[i]) == 0);
+		assert(getARCs(game, universities[i]) == 0);
+		assert(getGO8s(game, universities[i]) == 0);
+		assert(getCampuses(game, universities[i]) == 0);
+		assert(getIPs(game, universities[i]) == 0);
+		assert(getPublications(game, universities[i]) == 0);
+	}
+
 	int isLegalAction(Game g, action a);
 	action illegalAction1 = {OBTAIN_ARC, startLocA1, STUDENT_THD, STUDENT_THD};
 	assert(isLegalAction(game, illegalAction1) == FALSE);
 
-	assert(getTurnNumber(game) == -1);//When game is first gened turnNum should be -1
+	assert(getTurnNumber(game) == -1); // When game is first gened turnNum should be -1
+
+	// Throw a dice of 2
+
 	throwDice(game, 2);
 	assert(getTurnNumber(game) == 0);
 
-	
 
 	printf("All tests passed. You can kind of code :D\n");
 
