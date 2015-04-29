f785508dd8183e4f1ace7881c72e64872ece7a0c
TheBrainyOne <albert.chejin.edward.smith@outlook.com>
2015-04-29 22:33:56 +0800

    Include reference to external testState.h

--- testGame.c
+++ testGame.c
@@ -7,15 +7,7 @@
 #include <assert.h>
 
 #include "Game.h"
-
-<<<<<<< HEAD
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN    "\x1b[36m"
-#define RESET   "\x1b[0m"
+#include "testState.h"
 
 typedef _actionSet struct {
 	int action;
@@ -125,18 +117,4 @@ int main(int argc, char *argv[]){
 	scanf("%c", &temp);
 
 	return EXIT_SUCCESS;
-}
-
-void advAssert(Game game, int okay, char* description) {
-	if (!okay) {
-		printf(RED "------------------------------");
-		printf(RED "Assertion failed!\n");
-		printf(RED "Description: %s\n\n", description);
-		printf(RED "+-----------------+----------+");
-	}
-}
-
-
-void assertState(Game game, stateSet state) {
-
 }
