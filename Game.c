a790dd0413644f605c0b5275d8aea81b54b24a6c
happythe3 <alexshearer00@gmail.com>
2015-04-29 16:28:00 +0800

    Added pointers to _hex

--- Game.c
+++ Game.c
@@ -1,11 +1,66 @@
 //Game.c
 
+//----------#include-----------//
+
 #include "game.h"
+
+
+
+//----------#defines----------//
+
+
+
+//-----------Structs-----------//
+
+typedef struct _game game;
+typedef struct _hex hex;
+typedef struct _vert vert;
+typedef struct _edge edge;
+
+typedef struct _game { 
+	int currentTurn;
+	
+	int disciplines
+	... more stuff in here
+} game;
+
+
+typedef struct _hex {
+	int hexID; // 1-19 starting with 1 at top left
+	
+	hex *hexUp;
+	hex *hexDown;
+	hex *hexUpLeft;
+	hex *hexUpRight;
+	hex *hexDownLeft;
+	hex *hexDownRight;
+	
+	vert *vertUpLeft;
+	vert *vertUpRight;
+	vert *vertLeft;
+	vert *vertRight;
+	vert *vertDownLeft;
+	vert *vertDownRight;
+	
+	edge *edgeUp;
+	edge *edgeDown;
+	edge *edgeUpLeft;
+	edge *edgeUpRight;
+	edge *edgeDownLeft;
+	edge *edgeDownRight;
+} hex;
+
+
+//------------Main-------------//
+
+
 
 Game newGame(int discipline[], int dice[]){
 	Game game = { 0 };
 	return game;
 }
+
+//------------Building Map Functions--------//
 
 int getTurnNumber(Game g){ 
 	return -1; 
