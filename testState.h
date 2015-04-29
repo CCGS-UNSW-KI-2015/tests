8b64a3bb2e32763f3b4dfda6e4457c51ea9e209a
TheBrainyOne <albert.chejin.edward.smith@outlook.com>
2015-04-29 22:44:11 +0800

    Create testState.h
    Lalala

---
+++ testState.h
@@ -0,0 +1,27 @@
+/* testState.h
+ * Defines functions for testState.c
+ * Includes color ANSI escape sequences
+ */
+
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define BOLD    "\x1b[1m"
+
+#define RESET   "\x1b[0m"
+
+/* void advAssert(Game game, char* action, int expected, int got, char* helpText)
+ * A verbose assert that checks if expected is equal to got.
+ * If it is not, it prints a description, and dumps the current
+ * game state. (All of it.)
+ */
+void advAssert(Game game, char* action, int expected, int got, char* helpText);
+
+/* void assertState(Game game, stateSet state)
+ * Performs checks on the state
+ * (I think, correct this if I'm wrong)
+ */
+void assertState(Game game, stateSet state);
