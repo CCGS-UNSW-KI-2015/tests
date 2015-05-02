63e73095548f9d3c17c1164e591aecbe7ef2b6c6
Jason Chu <jasonchujc@gmail.com>
2015-05-02 20:33:50 +0800

    Re-add testGame.c?!?!??

---
+++ testGame.c
@@ -0,0 +1,80 @@
+/* testGame.c
+ * It's alive!
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include "testState.h"
+
+/*
+ * Function prototypes
+ */
+stateSet initState (stateSet state);
+
+int main(int argc, char *argv[]){
+	Game game;
+
+	//--------------TESTS-----------//
+
+	int defaultDis[] = DEFAULT_DISCIPLINES;
+	int defaultDice[] = DEFAULT_DICE;
+
+	game = newGame(defaultDis, defaultDice);
+	stateSet currentState;
+
+	printAction("Initializing game with default values", 0);
+
+	// Inititalise currentState game state
+	currentState = initState(currentState);
+
+	// Check it!
+	assertState(game, currentState);
+
+	/*
+	// Throw a dice of 2
+
+	throwDice(game, 2);
+	assert(getTurnNumber(game) == 0);
+	*/
+
+	printEnd();
+
+
+	return EXIT_SUCCESS;
+}
+
+stateSet initState (stateSet state) {
+	int defaultDis[] = DEFAULT_DISCIPLINES;
+	int defaultDice[] = DEFAULT_DICE;
+	int universities[] = {UNI_A, UNI_B, UNI_C};
+	int defaultSize = NUM_REGIONS;
+	int universitiesSize = 20;
+
+	int i = 0;
+	while (i < NUM_REGIONS) {
+		state.regions[i].numDiscipline = defaultDis[i];
+		state.regions[i].numDiceValue = defaultDice[i];
+
+		i++;
+	}
+
+	state.numMostARCs = NO_ONE;
+	state.numMostPublications = NO_ONE;
+	state.numTurnNumber = -1;
+	state.numWhoseTurn = NO_ONE;
+
+	i = 0;
+	while (i < PATH_LIMIT) {
+		state.paths[i].numCampus = VACANT_VERTEX;
+		state.paths[i].numARC = VACANT_ARC;
+
+		i++;
+	}
+
+	memset(state.unis, 0, sizeof(state.unis[0]) * universitiesSize);
+
+	return state;
+}
